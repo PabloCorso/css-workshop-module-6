9bdbfeacf4e1ab34b5af64fa607d77e05901e546
Pablo Corso <pabcorso@gmail.com>
2022-06-12 12:04:21 +0200

    3- Accessibility

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,12 @@ const Hero = () => {
             /images/hero-img@3x.jpg 3x
           `}
         />
-        <HeroImage src="/images/hero-img.jpg" />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="An example of the photos distributed through this site (cat on black background)"
+        />
       </picture>
-      <Swoop src="/swoop.svg" />
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
